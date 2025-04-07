3fcfa67dff7557fdc175708ed8e6ed26c4f3a86c
Adruzen <136687389+Adruzen@users.noreply.github.com>
2025-04-07 17:15:18 +0200

    Rewrite PluginInitializer with ProjectActivity interface

--- src/main/java/es/uma/patternhelper/PluginInitializer.java
+++ src/main/java/es/uma/patternhelper/PluginInitializer.java
@@ -1,50 +1,26 @@
 package es.uma.patternhelper;
 
-import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
 import es.uma.patternhelper.logger.Logger;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
 import java.io.IOException;
 
-@Service(Service.Level.APP)
-public final class PluginInitializer implements StartupActivity {
-
-    private static boolean isInitialized = false;
-
-    public PluginInitializer() throws IOException {
-        if (!isInitialized) {
-            isInitialized = true;
-
-            // Then get Logger instance
-            Logger logger = Logger.getInstance();
-            logger.info("Pattern Helper Plugin initialized.");
-        }
-    }
-
-    public static void initialize(File localProperties, File sharedProperties) throws IOException {
-        if (!isInitialized) {
-            isInitialized = true;
-            Logger.getInstance().info("Pattern Helper Plugin initialized with custom properties.");
-        }
-    }
-
-    public static PluginInitializer getInstance() {
-        return com.intellij.openapi.application.ApplicationManager.getApplication()
-                .getService(PluginInitializer.class);
-    }
+public final class PluginInitializer implements ProjectActivity {
 
     @Override
-    public void runActivity(@NotNull Project project) {
-        if (!isInitialized) {
-            isInitialized = true;
-        }
+    public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
+        Logger logger;
         try {
-            Logger.getInstance().info("Plugin initialized for project: " + project.getName());
+            logger = Logger.getInstance();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        logger.info("Pattern Helper Plugin initialized.");
+        return null;
     }
 }
